af1bec1ce6c132aff8526e09c0cb1848d4b63e09
Riley <rui.caig@northumbria.ac.uk>
2026-03-05 04:44:07 +0000

    Made kin list window

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 // Woodcutting thingie 
 
 let trees_chopped = 0;
-var snd = new Audio("assets/osaka_ah_sound.mp3")
 
 function chop() {
-     document.getElementById("woodguycutting").style.zIndex = "10";
+     document.getElementById("woodguy-cutting").style.zIndex = "10";
     setTimeout(function() {
         trees_chopped = trees_chopped + 1;
         document.getElementById("wood-count-text").innerText = `Total wood: ${trees_chopped}`
         console.log(trees_chopped)
-         document.getElementById("woodguycutting").style.zIndex = "-1";
+        document.getElementById("woodguy-cutting").style.zIndex = "-1";
     }, 2400);
 }
+
+
+// Osaka top bar button thingie
+
+var snd = new Audio("assets/osaka_ah_sound.mp3")
 
 function osaka_clicked() {
     snd.play()
