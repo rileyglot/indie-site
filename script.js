11460890123b19ed0dd4d77fd5859cf4acfd8a16
Riley <rui.caig@northumbria.ac.uk>
2026-03-05 03:33:49 +0000

    Added word counter, started top banner

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 // Woodcutting thingie 
+
+let trees_chopped = 0;
+var snd = new Audio("assets/osaka_ah_sound.mp3")
+
 function chop() {
      document.getElementById("woodguycutting").style.zIndex = "10";
     setTimeout(function() {
+        trees_chopped = trees_chopped + 1;
+        document.getElementById("wood-count-text").innerText = `Total wood: ${trees_chopped}`
+        console.log(trees_chopped)
          document.getElementById("woodguycutting").style.zIndex = "-1";
     }, 2400);
+}
+
+function osaka_clicked() {
+    snd.play()
+    // There should be more here soon I'm just tired
 }
