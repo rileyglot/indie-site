7f154ca1076e1966378da0e0756100dbe3a0eb84
Riley <rui.caig@northumbria.ac.uk>
2026-03-07 01:11:33 +0000

    Changed CSS to be much more responsive to different window sizes

--- script.js
+++ script.js
@@ -26,6 +26,7 @@ function osaka_clicked() {
 
 let bonnie_clicks = 1
 var bonnie_snd = new Audio("assets/YOU_BLINKED.MP3")
+bonnie_snd.volume = 0.4
 
 function bonnie_clicked() {
     if (bonnie_clicks < 10) {
@@ -36,6 +37,7 @@ function bonnie_clicked() {
         document.getElementById("blink-bottom").style.bottom = "0"
         setTimeout(function() {
         document.getElementById("bonnie").style.opacity = "0"
+        document.getElementById("bonnie").style.cursor = "default"
         document.getElementById("blink-top").style.top = "-50vh"
         document.getElementById("blink-bottom").style.bottom = "-50vh"
     }, 100)
