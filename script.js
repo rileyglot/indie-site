3f25ffd9251c6872a15bed7ab8d76b97e2c83ef8
Riley <rui.caig@northumbria.ac.uk>
2026-03-10 18:17:23 +0000

    Added osakalings and unlocking button game; positioning still needed

--- script.js
+++ script.js
@@ -15,12 +15,65 @@ function chop() {
 
 // Osaka top bar button thingie
 
-var osaka_snd = new Audio("assets/osaka_ah_sound.mp3")
+var osaka_snd = new Audio("assets/osaka-ah-sound.mp3")
+let osakalings_found = 0
 
 function osaka_clicked() {
-    osaka_snd.play()
-    // There should be more here soon I'm just tired
+    if (osakalings_found === 5) {
+        osaka_snd.play()
+    }
 }
+
+function osakaling_1_found() {
+    osakalings_found = osakalings_found + 1
+    document.getElementById("osakaling-counter").innerText = `${osakalings_found}/5`
+    document.getElementById("osakaling-1").style.opacity = 0
+    if (osakalings_found === 5) {
+        document.getElementById("osaka").style.filter = "grayscale(0)"
+        document.getElementById("osakaling-counter").style.color = "yellow"
+    }
+    }
+
+function osakaling_2_found() {
+    osakalings_found = osakalings_found + 1
+    document.getElementById("osakaling-counter").innerText = `${osakalings_found}/5`
+    document.getElementById("osakaling-2").style.opacity = 0
+    if (osakalings_found === 5) {
+        document.getElementById("osaka").style.filter = "grayscale(0)"
+        document.getElementById("osakaling-counter").style.color = "yellow"
+    }
+    }
+
+function osakaling_3_found() {
+    osakalings_found = osakalings_found + 1
+    document.getElementById("osakaling-counter").innerText = `${osakalings_found}/5`
+    document.getElementById("osakaling-3").style.opacity = 0
+    if (osakalings_found === 5) {
+        document.getElementById("osaka").style.filter = "grayscale(0)"
+        document.getElementById("osakaling-counter").style.color = "yellow"
+    }
+    }
+
+function osakaling_4_found() {
+    osakalings_found = osakalings_found + 1
+    document.getElementById("osakaling-counter").innerText = `${osakalings_found}/5`
+    document.getElementById("osakaling-4").style.opacity = 0
+    if (osakalings_found === 5) {
+        document.getElementById("osaka").style.filter = "grayscale(0)"
+        document.getElementById("osakaling-counter").style.color = "yellow"
+    }
+    }
+
+function osakaling_5_found() {
+    osakalings_found = osakalings_found + 1
+    document.getElementById("osakaling-counter").innerText = `${osakalings_found}/5`
+    document.getElementById("osakaling-5").style.opacity = 0
+    if (osakalings_found === 5) {
+        document.getElementById("osaka").style.filter = "grayscale(0)"
+        document.getElementById("osakaling-counter").style.color = "yellow"
+    }
+    }
+
 
 // YOU BLINKED.
 
